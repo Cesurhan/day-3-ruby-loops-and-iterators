2eca1d41b73026f2da376e48e0646de68cbbaf02
Cesurhan <ces-ur@hotmail.com>
2017-03-07 16:10:58 +0100

    Define the Recipe Steps

--- pancake.rb
+++ pancake.rb
@@ -42,3 +42,19 @@ loop do
     puts "I am sure you've done something wrong... How about another try?"
   end
 end
+# Defining the Recipe Steps
+steps = [
+  {description: "Sift the flour and salt into a mixing bowl and make a well in the centre.", action: "sift_flour_and_salt"},
+  {description: "Crack the egg into the well", action: "crack_eggs"},
+  {description: "Add the melted butter or oil and half the milk", action: "add_butter_and_milk"},
+  {description: "Draw the flour into the liquid by stirring all the time with a wooden spoon until all the flour has been incorporated and then beat well to make a smooth batter", action: "generic_recipe_step"},
+  {description: "Stir in the remaining milk.", action: "generic_recipe_step"},
+  {description: "Beat all the ingredients together for 1 minute in a blender or food processor", action: "generic_recipe_step"},
+  {description: "Leave to stand for about 30 minutes", action: "generic_recipe_step"},
+  {description: "To make the pancakes, heat a small heavy-based frying until very hot and then turn the heat down to medium.", action: "generic_recipe_step"},
+  {description: "Lightly grease with oil and then ladle in enough batter to coat the base of the pan thinly (about 2 tablsp.)", action: "generic_recipe_step"},
+  {description: ", tilting the pan so the mixture spreads evenly.", action: "generic_recipe_step"},
+  {description: "Cook over a moderate heat for 1-2 minutes or until the batter looks dry on the top and begins to brown at the edges.", action: "generic_recipe_step"},
+  {description: "Flip the pancake over with a palette knife or fish slice and cook the second side.", action: "generic_recipe_step"},
+  {description: "Turn onto a plate, smear with a little butter, sprinkle of sugar and a squeeze of lemon juice or why not try some of our other suggestions for sweet and savoury toppings.", action: "generic_recipe_step"},
+]
