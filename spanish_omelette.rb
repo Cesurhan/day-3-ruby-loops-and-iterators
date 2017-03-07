2bfb3263b909916571e478ca9674183e1b94bb94
Cesurhan <ces-ur@hotmail.com>
2017-03-07 15:32:08 +0100

    Defining the Ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -3,9 +3,18 @@ def print_divider
   puts "*" * 40
   puts "\n"
 end
-
 # Progress bar prints three times .'s with the delay of half second.
 def print_progress_bar
   3.times { sleep 0.5; print "." }
   puts "\n"
 end
+# Defining the ingredients
+NUM_EGGS = 1
+NUM_BUTTER = 1
+ingredients = [
+  {name: 'eggs', quantity: NUM_EGGS},
+  {name: 'butter', quantity: NUM_BUTTER},
+  {name: 'flour', quantity: '100g'},
+  {name: 'salt', quantity: '1pinch'},
+  {name: 'milk', quantity: '300ml'},
+]
